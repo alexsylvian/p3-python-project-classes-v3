54a93d43e666e1fb14c83b9711f9e20b54bf9e8f
alexsylvian <lazeratus1@gmail.com>
2024-02-27 20:09:15 -0500

    started the CLI

--- lib/cli.py
+++ lib/cli.py
@@ -2,7 +2,9 @@
 
 from helpers import (
     exit_program,
-    helper_1
+    submenu,
+    helper_physics,
+    helper_submenu_2
 )
 
 
@@ -13,15 +15,35 @@ def main():
         if choice == "0":
             exit_program()
         elif choice == "1":
-            helper_1()
+            classes()
+        elif choice == "2":
+            students()
         else:
             print("Invalid choice")
+
+def classes():
+    while True:
+        submenu()
+        submenu_choice = input(">> ")
+
+        if submenu_choice == "0":
+            break  # Go back to the main menu
+        elif submenu_choice == "1":
+            helper_physics()
+        elif submenu_choice == "2":
+            helper_submenu_2()
+        else:
+            print("Invalid choice")
+
+def students():
+    pass
 
 
 def menu():
     print("Please select an option:")
-    print("0. Exit the program")
-    print("1. Some useful function")
+    print("0. Exit the Program")
+    print("1. Classes Menu")
+    print("2. Students Menu")
 
 
 if __name__ == "__main__":
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,9 +1,47 @@
 # lib/helpers.py
 
-def helper_1():
-    print("Performing useful function#1.")
+def submenu():
+    print("Welcome to the Classes Menu. What Would You Like to do?")
+    print("1. List of Classes")
+    print("2. Create New Class")
+    print("3. Search for Class by Name")
+    print("0. Go back")
 
+def submenu_physics():
+    print("Welcome to Physics Class. What would you like to do?")
+    print("0. Go Back")
+
+def helper_physics():
+    while True:
+        submenu_physics()
+        submenu_physics_choice = input(">> ")
+        print("Welcome to Physics Class. What would you like to do?")
+        print("0. Go Back")
+        
+        if submenu_physics_choice == "0":
+            break
+
+
+def helper_submenu_2():
+    print("Performing Submenu Option 2")
+
+def classes():
+    while True:
+        submenu()
+        submenu_choice = input(">> ")
+
+        if submenu_choice == "0":
+            break  # Go back to the main menu
+        elif submenu_choice == "1":
+            helper_physics()
+        elif submenu_choice == "2":
+            helper_submenu_2()
+        else:
+            print("Invalid choice")
+
+def students():
+    pass
 
 def exit_program():
-    print("Goodbye!")
+    print("Exiting the program.")
     exit()
