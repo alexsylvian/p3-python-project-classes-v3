11e9cc4015bccf0b2b4bda914d2b5f456bb54eb8
alexsylvian <lazeratus1@gmail.com>
2024-02-27 21:33:18 -0500

    tentative CLI menu complete

--- lib/cli.py
+++ lib/cli.py
@@ -2,16 +2,15 @@
 
 from helpers import (
     exit_program,
-    submenu,
-    helper_physics,
-    helper_submenu_2
+    helper_class_template,
+    submenu_2
 )
 
 
 def main():
     while True:
         menu()
-        choice = input("> ")
+        choice = input(">> ")
         if choice == "0":
             exit_program()
         elif choice == "1":
@@ -23,17 +22,26 @@ def main():
 
 def classes():
     while True:
-        submenu()
-        submenu_choice = input(">> ")
-
-        if submenu_choice == "0":
+        submenu_classes()
+        choice = input(">> ")
+        if choice == "0":
             break  # Go back to the main menu
-        elif submenu_choice == "1":
-            helper_physics()
-        elif submenu_choice == "2":
-            helper_submenu_2()
+        elif choice == "1":
+            list_of_classes()
+        elif choice == "2":
+            submenu_2()
         else:
             print("Invalid choice")
+
+def list_of_classes():
+    while True:
+        submenu_list_of_classes()
+        choice = input(">> ")
+        
+        if choice == "0":
+            break
+        elif choice == "1":
+            helper_class_template()
 
 def students():
     pass
@@ -44,6 +52,37 @@ def menu():
     print("0. Exit the Program")
     print("1. Classes Menu")
     print("2. Students Menu")
+
+def submenu_classes():
+    print("Welcome to the Classes Menu. What would you like to do?")
+    print("1. List of Classes")
+    print("2. Create New Class")
+    print("3. Search for Class by Name")
+    print("0. Go back")
+
+def submenu_list_of_classes():
+    print(f"Current Classes are 'for class in classes' CLASSSLIST GOES HERE.") 
+    print("Please Select a Class")
+    print("for class in classes print etc")
+    print("0: Go Back")
+    print("1. TEMPLATE: PHYSICS")
+
+def submenu_class_template():
+    print("Welcome to x Class. What would you like to do?")
+    print("0. Go Back")
+    print("1. List of Students in this Class")
+    print("2. Update Class Name or Teacher")
+    print("3. Add Student to Class")
+    print("4. Delete Class")
+
+def sumbenu_students():
+    print("Welcome to the Students Menu. What would you like to do?")
+    print("0. Go Back")
+    print("1. List of Students")
+    print("2. Search for Student by Name")
+    print("3. Add New Student")
+    print("4. Update Student Information")
+    print("5. Expel Student")
 
 
 if __name__ == "__main__":
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,47 +1,13 @@
 # lib/helpers.py
 
-def submenu():
-    print("Welcome to the Classes Menu. What Would You Like to do?")
-    print("1. List of Classes")
-    print("2. Create New Class")
-    print("3. Search for Class by Name")
-    print("0. Go back")
-
-def submenu_physics():
-    print("Welcome to Physics Class. What would you like to do?")
-    print("0. Go Back")
-
-def helper_physics():
-    while True:
-        submenu_physics()
-        submenu_physics_choice = input(">> ")
-        print("Welcome to Physics Class. What would you like to do?")
-        print("0. Go Back")
-        
-        if submenu_physics_choice == "0":
-            break
-
-
-def helper_submenu_2():
+def submenu_2():
     print("Performing Submenu Option 2")
 
-def classes():
-    while True:
-        submenu()
-        submenu_choice = input(">> ")
-
-        if submenu_choice == "0":
-            break  # Go back to the main menu
-        elif submenu_choice == "1":
-            helper_physics()
-        elif submenu_choice == "2":
-            helper_submenu_2()
-        else:
-            print("Invalid choice")
-
-def students():
-    pass
+def helper_class_template():
+    print("Class is PHYSICS. TEACHER is LOL")
 
 def exit_program():
     print("Exiting the program.")
     exit()
+
+
