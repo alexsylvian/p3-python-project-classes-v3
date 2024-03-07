a64ec9ab442f22bff66b86786b83d40a34466e64
alexsylvian <lazeratus1@gmail.com>
2024-03-07 00:17:11 -0500

    cleanup

--- lib/cli.py
+++ lib/cli.py
@@ -2,15 +2,14 @@
 
 from helpers import (
     exit_program,
-    find_student_by_name,
+    create_initial_table, 
+    list_courses, 
     create_new_course,
-    create_initial_table,
-    create_course_list,
     find_course_by_subject,
-    list_courses,
-    create_new_student,
     create_initial__student_table,
     list_of_students,
+    find_student_by_name,
+    create_new_student,
     update_student_information,
     expel_student
 )
@@ -66,8 +65,8 @@ def students():
         elif choice == "5":
             expel_student()
 
-
-
+#####################
+            
 def menu():
     print("Please select an option:")
     print("0. Exit the Program")
@@ -80,22 +79,6 @@ def submenu_courses():
     print("2. Create New Course")
     print("3. Search for Course by Subject")
     print("0. Go back")
-
-def submenu_list_of_courses():
-    print(f"Current courses are 'for course in courses' courseSLIST GOES HERE.") 
-    print("Please Select a course")
-    print("for course in courses print etc")
-    print("0. Go Back")
-    print("1. TEMPLATE: PHYSICS")
-
-def submenu_course_template():
-    print("Welcome to x course. What would you like to do?")
-    print("0. Go Back")
-    print("1. List of Students in this Course")
-    print("2. Update Course Name or Teacher")
-    print("3. Add Student to Course")
-    print("4. Remove Student from Course")
-    print("5. Delete Course")
 
 def submenu_students():
     print("Welcome to the Students Menu. What would you like to do?")
